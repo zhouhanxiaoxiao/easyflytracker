cd98c2bc24f0a0a1f3f83b04119969179bd1b619
zhouhanxiaoxiao <981436939@qq.com>
2020-11-25 14:34:05 +0800

    initEasyFly

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from "@/router/router";
+import i18n from "@/i18n";
+import 'iview/dist/styles/iview.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import $ from "jquery";
+import iView from "iview";
+
+Vue.use(iView);
+
+Vue.prototype.$ = $;
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
+  i18n
 }).$mount('#app')
